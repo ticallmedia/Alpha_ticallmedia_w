80633c78dedd753f339b87ed2421588c482d3d3c
Cesar Chavarriaga <tronos_123@hotmail.com>
2025-12-16 00:56:13 -0500

    1 idioma 9

--- app.py
+++ app.py
@@ -377,10 +377,10 @@ def request1_messages(ESTADO_USUARIO, telefono_id, lang):
         telefono_id, 
         message_response_for_list, 
         'list', 
-        list_titles = ["DDA & Mobile 📱","Websites 🌐","Photography 📸",
-                       "Content Mktg ✍️","Media Strat 📈","Digital Mktg 💻",
-                       "Paid Social 📊","Ecommerce Strat 🛒","Display Media 📺",
-                       "Hablar con agente 🗣️"], # El titulo no debe superar 24 caracteres
+        list_titles = ["📱DDA & Mobile Campaigns","📊Display Media Planning","🛒Ecommerce Strategy",
+                       "📣Paid Social Media","🎯Audience Studies","🚀Digital Marketing",
+                       "📰Media Strategy","🤖Custom Bot Development","🌐WebSites",
+                       "🗣️Talk to an Agent"], # El titulo no debe superar 24 caracteres
         list_ids = ["btn_1","btn_2","btn_3","btn_4","btn_5","btn_6","btn_7","btn_8","btn_9","btn_0"],           # Pasamos los IDs fijos
         list_descrip=["DDA And Mobile Campaigns.","Desarrollo de sitios","Fotografía profesional para marcas",
                       "Estrategias de contenido digital","Planificación de medios digitales","Marketing digital multicanal",
